b8bd46b1b8c414333dc4db9c813e4aa226328e3f
Labiba Ibnat Matin <ibnatmatin30@gmail.com>
2025-08-11 12:55:47 +0600

    adding something

--- frontend/src/Components/Welcome.js
+++ frontend/src/Components/Welcome.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Welcome() {
     return (
         <div>
-            <h1>Welcome to Kicko</h1>
+            <h1>Welcome to Kicko from labiba   </h1>
             <pre>{`
                 ⠀⠀⠀⣀⡤⠤⢤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡴⠒⠒⠦⣄⡀⠀⠀⠀⠀
                 ⠀⢠⠞⠉⠠⡄⠀⢿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⡇⠀⡤⠆⠀⠹⣄⠀⠀⠀
