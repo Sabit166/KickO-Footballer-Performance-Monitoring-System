03a2330b40b2751d05066b759437e4ba9c4f80c6
MD TAMIMUL ISLAM <tamimulislan@gmail.com>
2025-08-16 01:43:44 +0600

    solved

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import { Fade } from '@mui/material';
 import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
-import Players from './Components/Players';
-import PlayerPerformance from './Components/PlayerPerformance'
-import Team from '.Components/Team'
 import './App.css';
 import HomePage from './Components/HomePage';
 import Login from './Components/NewLogin';
@@ -16,9 +12,6 @@ function App() {
        <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/players" element={<Players />} />
-        <Route path="/player_performance" element={<PlayerPerformance />} />
-        <Route path="/teams" element={<Team />} />
        </Routes>
      </Router>
   );
